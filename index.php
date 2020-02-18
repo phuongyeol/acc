97212f04bae91642f8791ee7e0497fb6c2461fdb
phuongtran <phuong.tran@base.vn>
2020-02-18 14:06:17 +0700

    index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?phsp
-    // Base account
+<?php
+    date_default_timezone_set('Asia/Ho_Chi_Minh');
+    session_start();
+
+    if (!isset($_SESSION['login']) && $_SESSION['login'] == true) {
+        include_once 'views/users/show.php';
+    } else {
+        include_once 'view/auth/login.php';
+    }
 ?>

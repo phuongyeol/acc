91665e8da99225720bc312ffb3bfa7803e1aecf5
phuongtran <phuong.tran@base.vn>
2020-02-18 17:35:59 +0700

    add style login.php

--- views/auth/login.php
+++ views/auth/login.php
@@ -4,65 +4,60 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Base Account</title>
+    <link rel="shortcut icon" href="public/base-icon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="public/css/login/login.css">
 </head>
 <body>
-    <div class="left-bar">
-        <div class="header"></div>
-            <div class="base-logo">
-                <img src="public/logo.full.png" alt="">
-            </div>
-            <div class="welcome">
-                <p><h3>Login</h3></p>
-                <p>Welcome back. Login to start working.</p>
-            </div>
+    <div id="login" class="full-height">
+        <div class="header">
+            <div class="base-logo"><img src="public/logo.full.png" alt=""></div>
+            <div class=""><h2>Login</h2></div>
+            <div class="welcome gray">Welcome back. Login to start working.</div>
         </div>
 
         <div class="body">
-            <div class="login-form">
-                <div class="login-email">
-                    <div class="email-title">
-                        <label for="">Email</label>
+            <form action="" method="post">
+                <div class="login-form">
+                    <div class="login-email row">
+                        <div class="title"><label for=""><b>Email</b></label></div>
+                        <div class="input">
+                            <input type="email" name="email" id="email" placeholder="Your email">
+                        </div>
                     </div>
-                    <div class="email-input">
-                        <input type="email" name="email" id="email">
+                    <div class="login-password row">
+                        <div class="title">
+                            <label for=""><b>Password</b></label>
+                            <a href="#"><span class="a right">Forget your password?</span></a>
+                        </div>
+                        <div class="input">
+                            <input type="password" name="password" id="password" placeholder="Your password">
+                        </div>
+                    </div>
+                    <div class="login-remember row">
+                        <input type="checkbox" name="remember" id="remember-me">
+                        <span class="gray">Keep me logged in</span>
+                    </div>
+                    <div class="login-submit row">
+                        <input type="submit" value="Login to start working">
                     </div>
                 </div>
-                <div class="login-password">
-                    <div class="password-title">
-                        <label for="">Password</label>
-                        <span>Forget your password?</span>
-                    </div>
-                    <div class="password-input">
-                        <input type="password" name="password" id="password">
-                    </div>
-                </div>
-            </div>
-            <div class="login-remember">
-                <input type="checkbox" name="remember" id="remember-me">
-                <span>Keep me logged in</span>
-            </div>
-            <div class="login-submit">
-                <button type="submit">Login to start working</button>
-            </div>
+            </form>
         </div>
-        
+
         <div class="footer">
-            <div class="title">
+            <div class="title gray">
                 <p>Or, login  via single sign-on</p>
             </div>
             <div class="login-with-app">
-                <button type="submit">Login with Google</button>
-                <button type="submit">Login with Microsoft</button>
-                <button type="submit">Login with SAML</button>
+                <input type="submit" value="Login with Google">
+                <input type="submit" value="Login with Microsoft">
+                <input type="submit" value="Login with SAML">
             </div>
             <div class="login-with-other">
-                <a href="#">Login with Guest/Client access?</a>
+                <a href="#" class="a">Login with Guest/Client access?</a>
             </div>
         </div>
-    </div>
-    
-    <div class="right-bar">
-        <img src="public/background.png" alt="">
     </div>
 </body>
 </html>

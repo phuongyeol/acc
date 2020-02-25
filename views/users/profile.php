a4d0eb84deeedf5b34877a246cdec1a7eef73443
phuongtran <phuong.tran@base.vn>
2020-02-25 16:46:02 +0700

    fix css

--- views/users/profile.php
+++ views/users/profile.php
@@ -6,7 +6,7 @@
     <title>Base Account</title>
     <link rel="shortcut icon" href="public/images/base-icon.png" type="image/x-icon">
     <link rel="stylesheet" href="public/css/style.css">
-    <link rel="stylesheet" href="public/css/profile.css">
+    <link rel="stylesheet" href="public/css/profile/profile.css">
     <!-- Load an icon library -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
